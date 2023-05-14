58555da7594374d8ab3ba42748a760d4b670faa8
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-14 22:18:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_pettern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            for (x = 1; x <= 6; x++)
+            {
+                for (y = 1; y <= x; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+        
+    
+
